93fc8c3ac22d49b549bcea558370cd241b551ee6
Glitch (gokus-slave) <none>
2024-11-23 16:55:41 +0000

    🏯🏌 Checkpoint ./bot.py:71661195/746

--- bot.py
+++ bot.py
@@ -493,6 +493,13 @@ async def save_daily_top_users(clan_role_1_id, clan_role_2_id):
             elif clan_role_2_id in roles:  # Check if user has clan role 2
                 save_to_clan_table("clan_role_2", user_id, xp)
 
+                async def reset_task_wrapper():
+    await reset_task(CLAN_ROLE_1_ID, CLAN_ROLE_2_ID)
+
+@tasks.loop(seconds=3600)
+async def reset_task_loop():
+    await reset_task_wrapper()
+
 # The reset task function (as an example)
 @tasks.loop(seconds=30)
 async def reset_task(CLAN_ROLE_1_ID, CLAN_ROLE_2_ID):
